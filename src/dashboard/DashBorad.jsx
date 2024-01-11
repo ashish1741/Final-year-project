a81af2212af280758162f7635ab9189e56a51f0b
Ashish poudel <techcode057@gmail.com>
2024-01-11 12:17:43 +0545

    completed

--- src/dashboard/DashBorad.jsx
+++ src/dashboard/DashBorad.jsx
@@ -1,8 +1,107 @@
 import React from 'react'
+import GolfCourseIcon from '@mui/icons-material/GolfCourse';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import IconButton from '@mui/material/IconButton';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
 
 function DashBorad() {
+  const courses = [
+    {
+      id: 1,
+      name: "Front-end Development",
+      price: "$120",
+      creationDate: "2023-02-19",
+      instructor: "John Doe",
+      duration: "8 weeks",
+      description: "Learn front-end technologies like HTML, CSS, and JavaScript.",
+      // Add more properties as needed
+    },
+    {
+      id: 2,
+      name: "Back-end Development",
+      price: "$150",
+      creationDate: "2023-02-21",
+      instructor: "Jane Smith",
+      duration: "10 weeks",
+      description: "Master back-end technologies such as Node.js and databases.",
+      // Add more properties as needed
+    },
+    // Add more courses as needed
+  ];
+  
+
   return (
-    <div>DashBorad</div>
+    <div className="bg-gray-900  w-full h-full overscroll-none">
+      <h1 className="text-gray-300 font-extrabold">Dashboard</h1>
+      <div className="mt-3 p-3 flex justify-around gap-3">
+        <div className="bg-slate-800 shadow-xl  rounded-md p-2">
+          <div className="flex justify-between text-gray-300 gap-3 p-2" >
+          <span>course  </span>
+          <GolfCourseIcon />
+          </div>
+          <div className="flex justify-between gap-3 p-2">
+          <span>course  </span>
+          <h2>40</h2>
+          </div>
+        </div>
+
+        <div className="bg-slate-800 shadow-xl  rounded-md p-2">
+          <div className="flex justify-between text-gray-300 gap-3 p-2" >
+          <span>Enrolled  </span>
+          <LocalOfferIcon />
+          </div>
+          <div className="flex justify-between gap-3 p-2">
+          <span>course  </span>
+          <h2>40</h2>
+          </div>
+        </div>
+
+        <div className="bg-slate-800 shadow-xl  rounded-md p-2">
+          <div className="flex justify-between text-gray-300 gap-3 p-2" >
+          <span>Earning  </span>
+          <AccountBalanceWalletIcon />
+          </div>
+          <div className="flex justify-between gap-3 p-2">
+          <span>course  </span>
+          <h2>40</h2>
+          </div>
+        </div>
+
+
+      </div>
+      <div className="mt-5 flex p-2">
+      {courses.map((course, index) => (
+        <div key={index} className="mt-4 p-3 border border-gray-400">
+          <h3 className='text-gray-400'>Trend Course</h3>
+          <div className="flex justify-between space-x-4 gap-3">
+            <div className="p-2">
+              <h1>Name</h1>
+              <span className='mt-3'>{course.name}</span>
+            </div>
+            <div className="p-2">
+              <h1>Price</h1>
+              <span className='mt-3'>{course.price}</span>
+            </div>
+            <div className="p-2">
+              <h1>Date of creation</h1>
+              <span className='mt-3'>{course.creationDate}</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <IconButton aria-label="edit">
+                <ModeEditIcon fontSize="small" />
+              </IconButton>
+              <IconButton aria-label="delete">
+                <DeleteForeverIcon fontSize="small" />
+              </IconButton>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+      </div>
   )
 }
 
--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -1,9 +1,141 @@
-import React from 'react'
+import React, { useState } from "react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import GetAppIcon from "@mui/icons-material/GetApp";
+import { useNavigate } from "react-router-dom";
 
 function Tools() {
+  const navigate = useNavigate();
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const handleFileChange = (event) => {
+    // Get the selected file
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const navigateToHome = () => {
+    navigate("/"); // Define the route to your home page
+  };
   return (
-    <div>Tools</div>
-  )
+    <div>
+      <div className="flex">
+        <h1
+          className="text-gray-500 p-2 font-bold cursor-pointer"
+          onClick={navigateToHome}
+        >
+          &#x2190; Back to Home
+        </h1>
+      </div>
+      <h1 className="text-gray-300 capitalize p-2 font-bold text-3xl  text-center">
+        {" "}
+        Tools Conversion utilitie{" "}
+      </h1>
+
+      <div className="flex justify-center items-center h-screen bg-gray-800">
+        <div className="max-w-4xl w-full grid grid-cols-2 gap-4 text-center p-3">
+          {/* for jpg to pdf */}
+          <div className=" bg-gray-700 rounded-md p-3 my-3 shadow-xl ">
+            <div className="m-1">
+              <span className="text-gray-400">
+                Convert Jpg images into Png formate
+              </span>{" "}
+              <br />
+              <input type="file" className="text-gray-500 p-2 m-3" onChange={handleFileChange}  />
+            </div>
+            <div className="flex space-x-3 text-center ">
+              <button
+                className="text-gray-400 bg-gray-800 p-3 rounded-md font-bold
+             hover:bg-transparent hover:border"
+              >
+                Preview <RemoveRedEyeIcon />{" "}
+              </button>
+              <button
+                className="text-gray-400 bg-transparent border p-3 
+              rounded-md font-bold hover:bg-gray-700 hover:border-none "
+              >
+                Download <GetAppIcon />
+              </button>
+            </div>
+          </div>
+
+          {/* for text to pdf  */}
+          <div className=" bg-gray-700 rounded-md p-3 my-3 shadow-xl  ">
+            <div className="m-1">
+              <span className="text-gray-400">
+                Convert text files into pdf formate
+              </span>{" "}
+              <br />
+              <input type="file"  onChange={handleFileChange}  className="text-gray-500 p-2 m-3" />
+            </div>
+            <div className=" flex space-x-3 text-center ">
+              <button
+                className="text-gray-400 bg-gray-800 p-3 rounded-md font-bold
+             hover:bg-transparent hover:border"
+              >
+                Preview <RemoveRedEyeIcon />{" "}
+              </button>
+              <button
+                className="text-gray-400 bg-transparent border p-3 
+              rounded-md font-bold hover:bg-gray-800 hover:border-none "
+              >
+                Download <GetAppIcon />
+              </button>
+            </div>
+          </div>
+
+          {/* for png tojpg file  */}
+          <div className="  bg-gray-700 rounded-md p-3 my-3 shadow-xl  ">
+            <div className="m-1">
+              <span className="text-gray-400">
+                Convert png images into jpg formate
+              </span>{" "}
+              <br />
+              <input  onChange={handleFileChange}  type="file" className="text-gray-500 p-2 m-3" />
+            </div>
+            <div className="   flex space-x-3 text-center mx-10 ">
+              <button
+                className="text-gray-400 bg-gray-800 p-3 rounded-md font-bold
+             hover:bg-transparent hover:border"
+              >
+                Preview <RemoveRedEyeIcon />{" "}
+              </button>
+              <button
+                className="text-gray-400 bg-transparent border p-3 
+              rounded-md font-bold hover:bg-gray-800 hover:border-none "
+              >
+                Download <GetAppIcon />
+              </button>
+            </div>
+          </div>
+
+          {/* // for pdf to text  */}
+          <div className=" bg-gray-700 rounded-md p-3 my-3 shadow-xl  ">
+            <div className="m-1">
+              <span className="text-gray-400">
+                Convert pdf files into pdf formate
+              </span>{" "}
+              <br />
+              <input onChange={handleFileChange}  type="file" className="text-gray-500 p-2 m-3" />
+            </div>
+            <div className="flex space-x-3 text-center ">
+              <button
+                className="text-gray-400 bg-gray-800 p-3 rounded-md font-bold
+             hover:bg-transparent hover:border"
+              >
+                Preview <RemoveRedEyeIcon />{" "}
+              </button>
+              <button
+                className="text-gray-400 bg-transparent border p-3 
+              rounded-md font-bold hover:bg-gray-800 hover:border-none "
+              >
+                Download <GetAppIcon />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Tools
+export default Tools;
